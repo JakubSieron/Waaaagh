d4515fd1be309ba31e86ad2edab13ec75a143c54
Edem <bewareships@gmail.com>
2025-04-14 17:06:33 +0100

    margins and paddings

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -5,11 +5,13 @@ import { FeaturedDeals } from '../features/home/ui/FeaturedDeals';
 import { PopularCategories } from '../features/home/ui/PopularCategories';
 import { Blog } from '../features/home/ui/Blog';
 import './Home.scss';
+import { Categories } from '@/features/home/ui/Categories';
 
 const Home: React.FC = () => {
   return (
     <div className="main-content">
       <Hero />
+      <Categories />
       <div className="container">
         <div className="left-column">
           <FeaturedDeals />
